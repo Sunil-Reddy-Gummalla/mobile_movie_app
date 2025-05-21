8a61159b8e5c3bc9a1d9a81cb82d5e15e0d4aadd
Venkata Sunil Reddy Gummalla <sunilreddy.gummalla@hidubai.com>
2025-05-21 17:30:59 +0400

    added home page and search and movie details page

---
+++ components/SearchBar.tsx
@@ -0,0 +1,35 @@
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
+
+interface Props {
+    placeholder: string;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    onPress?: () => void;
+  }
+
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="ab8bff"
+      ></Image>
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#A8B5DB"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({});
---
+++ services/api.ts
@@ -0,0 +1,51 @@
+export const TMBD_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMBD_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMBD_CONFIG.BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMBD_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+  const data = await response.json();
+
+  return data.results;
+};
+
+
+export const fetchMovieDetails = async (
+    movieId: string
+  ): Promise<MovieDetails> => {
+    try {
+      const response = await fetch(
+        `${TMBD_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMBD_CONFIG.API_KEY}`,
+        {
+          method: "GET",
+          headers: TMBD_CONFIG.headers,
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching movie details:", error);
+      throw error;
+    }
+  };
